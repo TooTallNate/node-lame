723fb23c597a4117a811ee5f981634cff87a1159
Nathan Rajlich <nathan@tootallnate.net>
2011-11-11 00:41:52 -0800

    Rewrite test3 more like an example, using id3 module.

--- test3.js
+++ test3.js
@@ -1,20 +1,40 @@
-var fs = require('fs')
+var id3 = require('id3')
   , Parser = require('./lib/parser')
 
-var f = fs.createReadStream('pigs.good.mp3')
+// Read an MPEG file from stdin
+var f = process.stdin;
+f.resume();
+
+// Create a Parser instance
 var parser = new Parser();
+
+// pipe stdin to the parser
 f.pipe(parser);
 
+// emits 'header' events on each MPEG header (start of frame)
 parser.on('header', function (b, meta) {
-  //console.error('header', meta);
+  console.error('header', meta);
   process.stdout.write(b);
-}).on('frame', function (b) {
-  //console.error('frame');
+});
+
+// emits 'frame' event for the actual data from each frame
+parser.on('frame', function (b) {
+  console.error('frame');
   process.stdout.write(b);
-}).on('id3v2', function (b) {
-  console.error('id3v2');
-  console.error(b.length, b.toString('binary', 0, 100));
-}).on('id3v1', function (b) {
-  console.error('id3v1');
-  console.error(b.length, b.toString());
+});
+
+// emits 'id3v2' when the ID3v2 section at the beginning has been parsed
+parser.on('id3v2', function (b) {
+  var tags = new id3(b);
+  console.error(tags.getTags());
+  process.stdout.write(b);
+  console.error('id3v2. %d bytes', b.length);
+});
+
+// emits 'id3v1' event when the ID3v1 section at the end has been parsed
+parser.on('id3v1', function (b) {
+  var tags = new id3(b);
+  console.error(tags.getTags());
+  process.stdout.write(b);
+  console.error('id3v1. %d bytes', b.length);
 });
