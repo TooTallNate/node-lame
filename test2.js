bec51f6f265f82550a02184b183bc6f95ffa085e
Nathan Rajlich <nathan@tootallnate.net>
2011-11-11 00:32:14 -0800

    Can't use BlockStream.
    It's a nice module though!

--- test2.js
+++ test2.js
@@ -1,14 +1,11 @@
 var lame = require('./')
   , fs = require('fs')
   , Parser = require('./lib/parser')
-  , BlockStream = require('block-stream')
 
 var f = fs.createReadStream('pigs.f.s16le.acodec.pcm_s16le.ar.44100.ac.2')
   , encoder = lame.createEncoder()
-  //, bs = new BlockStream(s, { nopad: true })
 
 f.pipe(encoder);
-//encoder.pipe(bs);
 
 var parser = new Parser();
 encoder.pipe(parser);
@@ -20,12 +17,3 @@ parser.on('header', function (b, meta) {
   console.error('frame');
   process.stdout.write(b);
 });
-
-
-
-/*bs.on('data', function (frame) {
-
-  console.error('got frame')
-  console.error(frame.length)
-  console.error(0, frame.slice(0, 4))
-});*/
