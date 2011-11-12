224959cd89464d39df3d4741b79b7430a8c1ebaf
Nathan Rajlich <nathan@tootallnate.net>
2011-11-12 10:39:11 -0800

    Start writing the async Encoder. Ughh such a PITA!

--- test/test-encoder.js
+++ test/test-encoder.js
@@ -1,18 +1,17 @@
 var lame = require('../')
   , fs = require('fs')
 
-var f = fs.createReadStream('pigs.f.s16le.acodec.pcm_s16le.ar.44100.ac.2')
+var f = fs.createReadStream(__dirname + '/../pigs.f.s16le.acodec.pcm_s16le.ar.44100.ac.2')
   , encoder = lame.createEncoder()
 
-f.pipe(encoder);
-
-var parser = lame.createParser();
-encoder.pipe(parser);
-
-parser.on('header', function (b, meta) {
-  console.error('header:', meta);
-  process.stdout.write(b);
-}).on('frame', function (b) {
-  console.error('frame');
-  process.stdout.write(b);
+f.on('data', function (b) {
+  //console.error('file data. %d bytes.', b.length);
+  var flushed = encoder.write(b, afterEncoderWrite);
+  //console.error('flushed?', flushed);
 });
+
+function afterEncoderWrite () {
+  console.error('afterEncoderWrite()');
+}
+
+encoder.pipe(process.stdout);
