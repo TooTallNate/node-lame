366a184559acb0bda59382fad94cddcc86814b8d
Nathan Rajlich <nathan@tootallnate.net>
2011-11-12 13:07:48 -0800

    Don't console.error() all over the damn place :)

--- test/test-encoder.js
+++ test/test-encoder.js
@@ -1,7 +1,8 @@
 var lame = require('../')
   , fs = require('fs')
+  , filename = process.argv[2] || __dirname + '/../pigs.f.s16le.acodec.pcm_s16le.ar.44100.ac.2';
 
-var f = fs.createReadStream(__dirname + '/../pigs.f.s16le.acodec.pcm_s16le.ar.44100.ac.2')
+var f = fs.createReadStream(filename)
   , encoder = lame.createEncoder()
 
 f.on('data', function (b) {
