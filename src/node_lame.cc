23da5df9baec7b7cb3a30f1fad06b551ac66a91d
Nathan Rajlich <nathan@tootallnate.net>
2011-11-15 18:19:06 -0800

    Use String::NewSymbol() for setting properties.

--- src/node_lame.cc
+++ src/node_lame.cc
@@ -60,6 +60,7 @@ Handle<Value> node_lame_close (const Arguments& args) {
   return Undefined();
 }
 
+/* called when a 'gfp' wrapper Object get's GC'd from JS-land */
 void gfp_weak_callback (Persistent<Value> wrapper, void *arg) {
   HandleScope scope;
   lame_global_flags *gfp = (lame_global_flags *)arg;
@@ -377,26 +378,26 @@ void InitLame(Handle<Object> target) {
   // Constants
   PropertyAttribute readonlydontdelete = static_cast<PropertyAttribute>(ReadOnly|DontDelete);
   // vbr_mode_e
-  target->Set(String::New("vbr_off"), Integer::New(vbr_off), readonlydontdelete);
-  target->Set(String::New("vbr_mt"), Integer::New(vbr_mt), readonlydontdelete);
-  target->Set(String::New("vbr_rh"), Integer::New(vbr_rh), readonlydontdelete);
-  target->Set(String::New("vbr_abr"), Integer::New(vbr_abr), readonlydontdelete);
-  target->Set(String::New("vbr_mtrh"), Integer::New(vbr_mtrh), readonlydontdelete);
-  target->Set(String::New("vbr_default"), Integer::New(vbr_default), readonlydontdelete);
+  target->Set(String::NewSymbol("vbr_off"), Integer::New(vbr_off), readonlydontdelete);
+  target->Set(String::NewSymbol("vbr_mt"), Integer::New(vbr_mt), readonlydontdelete);
+  target->Set(String::NewSymbol("vbr_rh"), Integer::New(vbr_rh), readonlydontdelete);
+  target->Set(String::NewSymbol("vbr_abr"), Integer::New(vbr_abr), readonlydontdelete);
+  target->Set(String::NewSymbol("vbr_mtrh"), Integer::New(vbr_mtrh), readonlydontdelete);
+  target->Set(String::NewSymbol("vbr_default"), Integer::New(vbr_default), readonlydontdelete);
   // MPEG_mode_e
-  target->Set(String::New("STEREO"), Integer::New(STEREO), readonlydontdelete);
-  target->Set(String::New("JOINT_STEREO"), Integer::New(JOINT_STEREO), readonlydontdelete);
-  target->Set(String::New("MONO"), Integer::New(MONO), readonlydontdelete);
-  target->Set(String::New("NOT_SET"), Integer::New(NOT_SET), readonlydontdelete);
+  target->Set(String::NewSymbol("STEREO"), Integer::New(STEREO), readonlydontdelete);
+  target->Set(String::NewSymbol("JOINT_STEREO"), Integer::New(JOINT_STEREO), readonlydontdelete);
+  target->Set(String::NewSymbol("MONO"), Integer::New(MONO), readonlydontdelete);
+  target->Set(String::NewSymbol("NOT_SET"), Integer::New(NOT_SET), readonlydontdelete);
   // Padding_type_e
-  target->Set(String::New("PAD_NO"), Integer::New(PAD_NO), readonlydontdelete);
-  target->Set(String::New("PAD_ALL"), Integer::New(PAD_ALL), readonlydontdelete);
-  target->Set(String::New("PAD_ADJUST"), Integer::New(PAD_ADJUST), readonlydontdelete);
+  target->Set(String::NewSymbol("PAD_NO"), Integer::New(PAD_NO), readonlydontdelete);
+  target->Set(String::NewSymbol("PAD_ALL"), Integer::New(PAD_ALL), readonlydontdelete);
+  target->Set(String::NewSymbol("PAD_ADJUST"), Integer::New(PAD_ADJUST), readonlydontdelete);
 
   // Maximum size of an album art
-  target->Set(String::New("MAXALBUMART"), Integer::New(LAME_MAXALBUMART), readonlydontdelete);
+  target->Set(String::NewSymbol("MAXALBUMART"), Integer::New(LAME_MAXALBUMART), readonlydontdelete);
   // This is deprecated I think...
-  target->Set(String::New("MAXMP3BUFFER"), Integer::New(LAME_MAXMP3BUFFER), readonlydontdelete);
+  target->Set(String::NewSymbol("MAXMP3BUFFER"), Integer::New(LAME_MAXMP3BUFFER), readonlydontdelete);
 
 
   // Functions
