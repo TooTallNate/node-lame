21437d6db54674cccc26c7bace4ef6d66908cb5f
Nathan Rajlich <nathan@tootallnate.net>
2012-10-14 19:26:15 -0700

    implement the Decoder class

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 /**
- * The `Encoder` outputs an MP3 file from PCM data written to it.
+ * The `Decoder` accepts an MP3 file and outputs raw PCM data.
+ */
+
+exports.Decoder = require('./lib/decoder');
+
+/**
+ * The `Encoder` accepts raw PCM data and outputs an MP3 file.
  */
 
 exports.Encoder = require('./lib/encoder');
