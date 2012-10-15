5472629637e1f9f54ed576023ffe7d6da9ca83de
Nathan Rajlich <nathan@tootallnate.net>
2012-10-15 10:39:55 -0700

    remove old test stuffs
    Gonna redo this in Mocha

--- test/test-decoder.js
+++
@@ -1,7 +0,0 @@
-var lame = require('../')
-  , fs = require('fs')
-  , assert = require('assert')
-
-fs.createReadStream(process.argv[2] || __dirname + '/../pigs.mp3')
-  .pipe(lame.createDecoder())
-  .pipe(process.stdout);
