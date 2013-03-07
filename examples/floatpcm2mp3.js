70906537e2ade35d18e3c124a3309a53e1e1883d
Nathan Rajlich <nathan@tootallnate.net>
2013-03-07 01:00:06 -0800

    examples: tweaks

--- examples/floatpcm2mp3.js
+++ examples/floatpcm2mp3.js
@@ -2,9 +2,9 @@ var fs = require('fs');
 var lame = require('../');
 var path = require('path');
 
-fs.createReadStream(path.resolve(__dirname, 'sample.float.pcm'))
-  .pipe(new lame.Encoder({ bitDepth: 32, float: true }))
+fs.createReadStream(process.argv[2] || path.resolve(__dirname, 'sample.float.pcm'))
+  .pipe(new lame.Encoder({ channels: 2, bitDepth: 32, float: true }))
   .pipe(fs.createWriteStream(path.resolve(__dirname, 'sample_pcm.mp3')))
   .on('close', function () {
-    console.error('done!')
+    console.error('done!');
   });
